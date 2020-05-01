ee8062d6bb71bc9c5ad72013e505af22db29abd5
BetsonWick <strelnikovila@gmail.com>
2020-05-02 01:46:19 +0700

    Initial commit

---
+++ app/src/main/java/wasteed/project/gamejamproject/MainActivity.java
@@ -0,0 +1,14 @@
+package wasteed.project.gamejamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
