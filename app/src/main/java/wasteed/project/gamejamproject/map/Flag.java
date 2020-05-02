ee91a392b5c823cdb11b7addf87a3e376b579060
Mr3zee <284627@niuitmo.ru>
2020-05-03 00:21:20 +0300

    lays for towers && not repetitive flags

--- app/src/main/java/wasteed/project/gamejamproject/map/Flag.java
+++ app/src/main/java/wasteed/project/gamejamproject/map/Flag.java
@@ -3,12 +3,29 @@ package wasteed.project.gamejamproject.map;
 import java.util.Random;
 
 public enum Flag {
-    Red, Green, Blue;
+    Red(false), Green(false), Blue(false), Invalid(false);
+
+    boolean taken;
+
+    private static int amountTaken = 0;
+
+    Flag(boolean taken) {
+        this.taken = taken;
+    }
 
     public static Flag[] flags = {Blue, Red, Green};
 
     public static Flag randomFlag() {
+        if (amountTaken == flags.length) {
+            return Invalid;
+        }
         Random random = new Random();
-        return flags[random.nextInt(flags.length)];
+        Flag flag;
+        do {
+            flag = flags[random.nextInt(flags.length)];
+        } while (flag.taken);
+        flag.taken = true;
+        amountTaken++;
+        return flag;
     }
 }
